eb808f531dcd1df529bf823da0f845cba220f2ed
lichunyang <lichunyang@tanmarket.cn>
2022-01-13 23:30:09 +0800

    axios params handle

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,53 +1,54 @@
-import { isDate, isObject } from "./util"
+import { isDate, isObject } from './util'
 
 function encode(val: string): string {
-    return encodeURIComponent(val)
-        .replace(/%40/g, '@')
-        .replace(/%3A/gi, ':')
-        .replace(/%24/g, '$')
-        .replace(/%2C/gi, ',')
-        .replace(/%20/g, '+')
-        .replace(/%5B/gi, '[')
-        .replace(/%5D/gi, ']')
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
 }
 
 export function buildUrl(url: string, params?: any) {
-    if (!params) {
-        return url
+  if (!params) {
+    return url
+  }
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    let val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
     }
-    const parts: string[] = []
+    let values: any[]
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
 
-    Object.keys(params).forEach((key) => {
-        let val = params[key]
-        if (val === null || typeof val === 'undefined') {
-            return
-        }
-        let values: string[]
-        if (Array.isArray(val)) {
-            values = val
-            key += '[]'
-        } else {
-            values = [val]
-        }
-
-        values.forEach((val) => {
-            if (isDate(val)) {
-                val = val.toISOString()
-            } else if (isObject(val)) {
-                val = JSON.stringify(val)
-            }
-            parts.push(`${encode(key)}=${encode(val)}`)
-        })
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
     })
+  })
 
-    let serializedParmas = parts.join('&')
+  let serializedParmas = parts.join('&')
 
-    if (serializedParmas) {
-        const markIndex = url.indexOf('#')
-        if (markIndex !== -1) {
-            url = url.slice(0, markIndex)
-        }
+  if (serializedParmas) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
     }
-    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParmas
+  }
+  url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParmas
 
+  return url
 }
